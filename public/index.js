012af0821562674fbd356e76bbff3019006b20fc
up2018781 <91746177+UP2018781@users.noreply.github.com>
2022-02-12 23:28:09 +0000

    laptop > pc

--- public/index.js
+++ public/index.js
@@ -1,13 +1,8 @@
+import { LoadItemsPage } from './pageSwap.js';
+import { LoadBasketPage } from './pageSwap.js';
+
 const bricks = document.querySelector('#bricks');
 const basket = document.querySelector('#basket');
-
-function LoadItemsPage() {
-  document.location.href = 'items.html';
-}
-
-function LoadBasketPage() {
-  document.location.href = 'basket.html';
-}
 
 bricks.addEventListener('click', () => {
   LoadItemsPage();
