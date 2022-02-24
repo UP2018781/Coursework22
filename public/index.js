f37dc2970b86849a1ce1ae6e73f29b590befd1ab
up2018781 <91746177+UP2018781@users.noreply.github.com>
2022-02-24 12:50:19 +0000

    added page functionality

--- public/index.js
+++ public/index.js
@@ -1,5 +1,12 @@
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector('.navbar');
 
-window.onload = () => {
-  console.log('page is fully loaded');
-};
+navbar.addEventListener('click', (e) => {
+  if (e.target.id !== 'index' && e.target.id !== 'sets' && e.target.id !== 'bricks' && e.target.id !== 'basket') {
+    window.location.hash = 'contact';
+  } else {
+    window.location.href = `${e.target.id}.html`;
+    const currentPage = `${e.target.id}`;
+    const highlightMe = document.querySelector(`#${currentPage}`);
+    highlightMe.style.backgroundColor = 'green';
+  }
+});
